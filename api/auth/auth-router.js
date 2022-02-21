08a0e74a93245c6704825ad5ef8198452af9a198
bugsbrog <bugsbrog@gmail.com>
2022-02-21 15:35:26 -0700

    Done with register endpoint. Now just have to write out checkUsernameExists mw

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,8 +8,16 @@ const {
 const bcrypt = require('bcryptjs')
 const Auth = require('../users/users-model')
 
-router.post('/register', async (req, res, next) => {
-
+router.post('/register', checkUsernameFree, async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8)
+    const user = { username, password: hash }
+    const createUser = await Auth.add(user)
+    res.json(createUser)
+  } catch (err) {
+      next(err)
+  }
 })
 
 /**
